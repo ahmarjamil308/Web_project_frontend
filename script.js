c02528275a276e5f1c05046cc22ef2be3fbc682b
Ahmar Jameel <130285644+ahmarjamil308@users.noreply.github.com>
2025-04-30 10:14:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function login() {
+    window.location.href = "login.html"; 
+}
+function joinUs() {
+    window.location.href = "join_us.html"; 
+}
+function buyerSignUp() {
+    window.location.href = "buyer_sign_up.html"; 
+}
+
+function buyerSignIn() {
+    window.location.href = "buyer_sign_in.html"; 
+}
+
